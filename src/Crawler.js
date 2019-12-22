646f944b54e66a8fc9781dfa0d28d89524de5a76
Edoardo Gargano <cnt000@gmail.com>
2019-12-22 13:10:36 +0100

    refactor: linting

--- src/Crawler.js
+++ src/Crawler.js
@@ -1,8 +1,14 @@
-const Crawler = async (delay, pagesList, asyncFunc, filenameFunc, crawlFunc) => {
+const Crawler = async (
+  delay,
+  pagesList,
+  asyncFunc,
+  filenameFunc,
+  crawlFunc,
+) => {
   for (let url of pagesList) {
     // FIXME inject = or / from caller
     const id = /=/.test(url) ? url.split('=').pop() : url.split('/').pop();
-      await asyncFunc(delay, url, filenameFunc(id), crawlFunc);
+    await asyncFunc(delay, url, filenameFunc(id), crawlFunc);
   }
 };
 
