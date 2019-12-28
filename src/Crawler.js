fff3ee3f3e6a20d861696b2aee925ad1355893c5
Edoardo Gargano <cnt000@gmail.com>
2019-12-28 13:05:07 +0100

    feat: delayed with setInterval

--- src/Crawler.js
+++ src/Crawler.js
@@ -1,14 +1,14 @@
-const Crawler = async (
-  pagesList,
-  asyncFunc,
-  filenameFunc,
-  crawlFunc,
-) => {
-  for (let url of pagesList) {
-    // FIXME inject = or / from caller
+const Crawler = async (pagesList, asyncFunc, filenameFunc, crawlFunc) => {
+  console.time();
+  const intervalId = setInterval(async () => {
+    const url = pagesList.pop();
     const id = /=/.test(url) ? url.split('=').pop() : url.split('/').pop();
+    console.timeLog(url);
     await asyncFunc(url, filenameFunc(id), crawlFunc);
-  }
+    if (pagesList.length === 0) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
 };
 
 module.exports = Crawler;
